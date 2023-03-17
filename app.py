8b32ba04985309e26592bd84258bd0a48b6af923
sudojoseph <josephdavidson1989@gmail.com>
2023-03-17 10:56:59 +0200

    quick fix

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 from flask import Flask, request
+import os
 from jarvis import Jarvis
 
 
@@ -11,6 +12,7 @@ def index():
     response = jarvis.ask(request_data['question'])
     return response
 
+port = int(os.environ.get('PORT', 5000))
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port='5000')
+    app.run(host='0.0.0.0', port=port)
