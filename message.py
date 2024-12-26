a3598072f5734f960687e7b00d99d58f4e3cb853
sudojoseph <josephdavidson1989@gmail.com>
2024-12-26 23:29:50 +0200

    integrating telegram logging micro service

--- message.py
+++ message.py
@@ -1,13 +1,17 @@
-import telepot
 import os
-import asyncio
+import requests
 
 
 class Telegram:
-    def __init__(self):
-        self.bot = telepot.Bot(os.getenv("TELEGRAM_BOT_TOKEN"))
-
-
     def send(self, message):
-        self.bot.sendMessage(os.getenv("TELEGRAM_BOT_CHAT_ID"), message)
+        URL = 'https://telegram-loggin-microservice-05d27d17ae1b.herokuapp.com/'
+        json = {"telegramBotToken": os.getenv("TELEGRAM_BOT_TOKEN"),
+                "chatId": os.getenv("TELEGRAM_BOT_CHAT_ID"),
+                "message": message}
+        headers = {'Content-Type': 'application/json',
+                         'x-api-key': os.getenv('TELEGRAM_LOGGER_API_KEY')}
+        try:
+            requests.post(URL, json=json, headers=headers)
+        except Exception as e:
+            print(e)
         return 'success'
